de937e9bef100200b377033e4e9dec908a804b76
McDaniels Okoro <mcdaniels.okoro@gmail.com>
2018-04-14 12:44:27 +0100

    Made UI Changes

--- js/app.js
+++ js/app.js
@@ -12,3 +12,41 @@
 //         reader.readAsDataURL(input.files[0]);
 //     }
 // }
+
+// window.onload = function() {
+//     var showAddButton = document.getElementById("showAdd");
+//     var addButton = document.getElementById("addContact");
+//     var cancelButton = document.querySelector("addForm");
+    
+//     var name = document.getElementById('name');
+//     var email = document.getElementById('email');
+//     var image = document.getElementById('image');
+//     var mobile = document.getElementById('mobile');
+// }
+
+// function addContact() {
+//         // Create a new contact object with the user's input.
+//     var contact = {
+//         "name": document.getElementById('name').value,
+//         "email": document.getElementById('email').value,
+//         "image": document.getElementById('image').value,
+//         "mobile": document.getElementById('mobile').value
+//     };
+// }
+
+function removeContact() {
+
+}
+
+function viewContacts() {
+    document.getElementById('contacts').style.display = "block";
+}
+
+function addContacts() {
+    document.getElementById('add-contacts').style.display = "block";
+}
+
+function hideContacts() {
+    document.getElementById('contacts').style.display = "none";
+    document.getElementById('add-contacts').style.display = "none";
+}
